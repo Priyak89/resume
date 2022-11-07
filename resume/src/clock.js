aa394c3827b2b394a28619a6fb71bf0dbc733aff
Priya <priya.kumaravelan@gmail.com>
2022-11-07 12:11:13 -0600

    clock_lifecycle

---
+++ resume/src/clock.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+class Clockdisp extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {date: new Date()};
+    }
+  
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+  
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });
+    }
+  
+    render() {
+      return (
+        <div>
+          <h1>This is the new clock.</h1>
+          <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+        </div>
+      );
+    }
+  }
+  export default Clockdisp;
