f63269e7370608b484b1faaadede1a2e3b7b3ecc
Priya <priya.kumaravelan@gmail.com>
2022-11-30 10:52:05 -0600

    customizing

--- resume/src/calculatorapp.js
+++ resume/src/calculatorapp.js
@@ -35,69 +35,154 @@ function MyCalculator() {
   return (
     <div>
       <h3>My Calculator</h3>
-      <input
-        placeholder="First Number"
-        type="number"
-        value={number1}
-        onChange={(e) => setNumber1(e.target.value)}
-      ></input>
-      <input
-        placeholder="Second Number"
-        type="number"
-        value={number2}
-        onChange={(e) => setNumber2(e.target.value)}
-      ></input>
-      <input
-        placeholder="Result"
-        type="number"
-        value={result}
-        disabled={true}
-      ></input>
-      <h2>Result:{result || ""}</h2>
-      <input
-        type="radio"
-        name="calc"
-        id="number1"
-        value="number1"
-        onChange={() => setButtonpress(1)}
-      ></input>
-      <label for="number1">Number 1 </label>
-      <br></br>
-      <input
-        type="radio"
-        name="calc"
-        id="number2"
-        value="number2"
-        onChange={() => setButtonpress(2)}
-      ></input>
-      <label for="number2">Number 2 </label>
-      <br></br>
-      <button onClick={Addtion}>Add</button>
-      <br></br>
-      <button onClick={Subtraction}>Subtract</button>
-      <br></br>
-      <button onClick={Multiplication}>Multiply</button>
-      <br></br>
-      <button onClick={Division}>Divide</button>
-      <br></br>
-      <button onClick={Percentage}>Percentage</button>
-      <br></br>
-      <button onClick={Reset}>Reset</button>
-      <br></br>
-      <p class="round1">
-        <button onClick={() => ClickHandler(0)}>0</button>
-        <button onClick={() => ClickHandler(1)}>1</button>
-        <button onClick={() => ClickHandler(2)}>2</button>
-        <button onClick={() => ClickHandler(3)}>3</button>
-        <br></br>
-        <button onClick={() => ClickHandler(4)}>4</button>
-        <button onClick={() => ClickHandler(5)}>5</button>
-        <button onClick={() => ClickHandler(6)}>6</button>
-        <br></br>
-        <button onClick={() => ClickHandler(7)}>7</button>
-        <button onClick={() => ClickHandler(8)}>8</button>
-        <button onClick={() => ClickHandler(9)}>9</button>
-      </p>
+      <table>
+        <tr>
+          <td>
+            <input
+              placeholder="First Number"
+              type="number"
+              value={number1}
+              onChange={(e) => setNumber1(e.target.value)}
+            ></input>
+          </td>
+
+          <td>
+            <input
+              placeholder="Second Number"
+              type="number"
+              value={number2}
+              onChange={(e) => setNumber2(e.target.value)}
+            ></input>
+          </td>
+          <td>
+            <input
+              placeholder="Result"
+              type="number"
+              value={result}
+              disabled={true}
+            ></input>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2">
+            <input
+              class="row2"
+              type="radio"
+              name="calc"
+              id="number1"
+              value="number1"
+              onChange={() => setButtonpress(1)}
+            ></input>
+
+            <label for="number1">Number 1 </label>
+          </td>
+          <td colspan="2">
+            <input
+              class="row2"
+              type="radio"
+              name="calc"
+              id="number2"
+              value="number2"
+              onChange={() => setButtonpress(2)}
+            ></input>
+
+            <label for="number2">Number 2 </label>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button class="row3" onClick={Addtion}>
+              +
+            </button>
+          </td>
+          <td>
+            <button class="row3" onClick={Subtraction}>
+              -
+            </button>
+          </td>
+          <td>
+            <button class="row3" onClick={Multiplication}>
+              *
+            </button>
+          </td>
+
+          <td>
+            <button class="row3" onClick={Division}>
+              /
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td colSpan="2">
+            <button class="row4" onClick={Percentage}>
+              %
+            </button>
+          </td>
+          <td colSpan="2">
+            <button class="row4" onClick={Reset}>
+              R
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="1.5">
+            <button class="row4" onClick={() => ClickHandler(1)}>
+              1
+            </button>
+          </td>
+          <td colspan="1.5">
+            <button class="row4" onClick={() => ClickHandler(2)}>
+              2
+            </button>
+          </td>
+          <td colspan="1.5">
+            <button class="row4" onClick={() => ClickHandler(3)}>
+              3
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(4)}>
+              4
+            </button>
+          </td>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(5)}>
+              5
+            </button>
+          </td>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(6)}>
+              6
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(7)}>
+              7
+            </button>
+          </td>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(8)}>
+              8
+            </button>
+          </td>
+          <td>
+            <button class="row4" onClick={() => ClickHandler(9)}>
+              9
+            </button>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="4">
+            <button class="row0" onClick={() => ClickHandler(0)}>
+              0
+            </button>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
